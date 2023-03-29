774005ff880136397519b119b41e4615a4c9a3a4
bhavesh <bhaveshchoudhary@9211gmail.com>
2023-03-29 14:58:49 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com/rightmasterr" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/iam_bhaveshh___/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Bhaveshhhhhhh" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
